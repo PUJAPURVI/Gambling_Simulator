fbb8b014fbdd4e0b4ffe915d7623256dcdf24951
PUJAPURVI <nerkarpuja@gmail.com>
2023-02-07 18:41:58 +0530

    Added: Daily Stacks

---
+++ GamblingGame.java
@@ -0,0 +1,10 @@
+package com.Review;
+
+public class GamblingGame {
+    public static final int stack = 100;
+    public static final int bet = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the game");
+    }
+}
